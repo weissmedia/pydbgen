30224a5acfd8fa545e4e8b4ae3c9f092886fded8
nobody anonymous <ddd.coredomain@gmail.com>
2021-09-05 22:16:19 +0200

    add versions

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['Faker', 'Pandas', 'pysqlite3', 'openpyxl'],  # Optional
+    install_requires=['Faker=>8.12', 'Pandas>=1.3', 'pysqlite3', 'openpyxl>=3.0'],  # Optional
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.
